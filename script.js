fbcff5aef972edcdf0f2e209416c88d32a9d2cf1
Max Goryunov <maximthehedgehog@yandex.ru>
2018-08-02 08:25:42 +0300

    options feature added

--- script.js
+++ script.js
@@ -40,5 +40,16 @@ function showTabsContent(b) {
 
 
 
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body, html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+
 
 
